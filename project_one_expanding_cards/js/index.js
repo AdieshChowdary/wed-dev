92125c4b26a667de7d79f1284e27054cf4d8a57e
Pulla <Adiesh.Pulla@Honeywell.com>
2024-06-18 15:08:01 +0530

    project one expanding cards is completed as at start a small confusion but its fine i guess have a revision if possible

--- project_one_expanding_cards/js/index.js
+++ project_one_expanding_cards/js/index.js
@@ -1,1 +1,15 @@
-alert("hi");
+// alert("hi");
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+        removeActiveClass();
+        panel.classList.add("active");
+    });
+}); 
+
+function removeActiveClass() {
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+    });
+}
